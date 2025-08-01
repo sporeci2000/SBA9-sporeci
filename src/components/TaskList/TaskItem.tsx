43f47cbe20c8b4525ff2883ab120d78a89c6cfd2
Sintja <sporeci2000@gmail.com>
2025-07-31 20:41:49 -0400

    Task Item completed

--- src/components/TaskList/TaskItem.tsx
+++ src/components/TaskList/TaskItem.tsx
@@ -4,16 +4,19 @@ import './TaskItem.css';
 
 interface TaskItemProps {
     task: Task;
-    onDelete: (id: string) => void;
-    onEdit: (task: Task) => void;
+    onDelete: (id: string) => void; //function to delete a task by id
+    onEdit: (task: Task) => void;  //function to start editing a task
 }
 
 export const TaskItem: React.FC<TaskItemProps> = ({ task, onDelete, onEdit }) => (
-    <li className={`${task.priority} ${task.status}`}>
-        <strong>{task.title}</strong> — {task.status} — {task.priority}
+    <li className={`${task.priority} ${task.status}`}> {/*Renders one task item */}
+
+        <strong>{task.title}</strong> | Status: {task.status} | Priority: {task.priority}
         <br />
+
         Description: {task.description || 'No description'}
         <br />
+
         Due Date: {task.dueDate || 'No due date'}
         <br />
         <button onClick={() => onEdit(task)}>Edit</button>
